9d987338499593b7cfcb7863f8d7d23549833ab8
KerryAndres1974 <bustoscruzandresfelipe@gmail.com>
2023-11-18 11:38:06 -0500

    Se agrega mas contenido a la pagina principal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Encabezado from './componentes/Encabezado.jsx';
 import videoBg from './multimedia/videofondo.mp4';
+import Proyecto from './componentes/Proyectos.jsx';
 import './App.css';
 
 export function App() {
@@ -12,13 +13,57 @@ export function App() {
           <h1>¡Bienvenido a Home Hub!</h1>
           <p>Hacemos realidad tus metas 
           inmobiliarias con profesionalismo y dedicación. ¡Encuentra propiedades que se adaptan a tu 
-          estilo de vida y objetivos!</p>
+          estilo de vida!</p>
         </div>
       </section>
       <section className='contenedorPropiedades'>
+        <p className='linea'></p>
         <h2>Proyectos para tu Familia</h2>
+        <p className='linea'></p>
+        
         <div className='contenedorProyectos'>
-          
+          <Proyecto
+            imagen='casa1'
+            titulo='Casa en Venta'
+            textos='En esta sección se muestran los proyectos que he 
+            realizado en mi vida profesional y académica'
+            precio='Por un precio de 120000000$'
+          />
+          <Proyecto
+            imagen='casa2'
+            titulo='Casa en Venta'
+            textos='En esta sección se muestran los proyectos que he 
+            realizado en mi vida profesional y académica'
+            precio='Por un precio de 120000000$'
+          />
+          <Proyecto
+            imagen='casa3'
+            titulo='Casa en Venta'
+            textos='En esta sección se muestran los proyectos que he 
+            realizado en mi vida profesional y académica'
+            precio='Por un precio de 120000000$'
+          />
+          <Proyecto
+            imagen='casa4'
+            titulo='Casa en Venta'
+            textos='En esta sección se muestran los proyectos que he 
+            realizado en mi vida profesional y académica'
+            precio='Por un precio de 120000000$'
+          />
+          <Proyecto
+            imagen='casa5'
+            titulo='Casa en Venta'
+            textos='En esta sección se muestran los proyectos que he 
+            realizado en mi vida profesional y académica'
+            precio='Por un precio de 120000000$'
+          />
+          <Proyecto
+            imagen='casa6'
+            titulo='Casa en Venta'
+            textos='En esta sección se muestran los proyectos que he 
+            realizado en mi vida profesional y académica'
+            precio='Por un precio de 120000000$'
+          />
         </div>
       </section>
     </div>
--- src/componentes/Proyectos.jsx
+++ src/componentes/Proyectos.jsx
@@ -1,12 +1,17 @@
 import '../hojasEstilos/Proyectos.css';
-/*https://jminmobiliaria.co/proyectos-en-venta/*/
-function Proyectos() {
+/*https://jminmobiliaria.co/proyectos-en-venta/
+https://www.realtor.com/international/co/house/*/
+function Proyectos( props ) {
     return (
-        <div className='contenedor'>
-            <image src='https://i.ibb.co/0jZ3Q0H/Proyectos.png' alt='Proyectos' className='imagen' />
-            <div className='texto'>
-                <h1>Proyectos</h1>
-                <p>En esta sección se muestran los proyectos que he realizado en mi vida profesional y académica.</p>
+        <div className='contenedor-propiedad'>
+            <img
+                className='imagen-propiedad' 
+                src={require(`../multimedia/${props.imagen}.jpg`)} 
+                alt='Foto propiedad' />
+            <div className='contenedor-texto-propiedad'>
+                <p className='titulo-propiedad'>{props.titulo}</p>
+                <p className='textos-propiedad'>{props.textos}</p>
+                <p className='precio-propiedad'>{props.precio}</p>
             </div>
         </div>
     );
